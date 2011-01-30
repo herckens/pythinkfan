94995427fde37abcd7f0e2bc6ebb0abc59168b9f
hendrik <he_list@gmx.de>
2011-01-30 20:39:38 +0100

    only using ibm/thermal now

--- he_pythinkfan.py
+++ he_pythinkfan.py
@@ -13,25 +13,18 @@ deamonTIME=5.0 # in [s] sleep time between samples
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
  
 while 1:
-        temp = []
-        # check sensors in laptop # not systen specific! great
-####   cat /proc/acpi/ibm/thermal | awk '{ for (i=2; i<=NF; i++) printf("%s\n" , $i)}'
-        st1="cat /proc/acpi/ibm/thermal | awk '{ for (i=2; i<=NF; i++) printf("
-        st1=st1+('"%s\\n" , $i)}')+("'")
-        f=os.popen(st1)
-        for i in f.readlines():
-                intw = float(i)
-                temp.append(intw)
-        #print temp
-        #  check cpu sensors # not systen specific! great
-        f=os.popen("sensors 2>/dev/null | grep Core | awk '{print $3}' | sed s/+// | sed 's/..$//'")
-        for i in f.readlines():
-                intw = float(i)
-                temp.append(intw)
-        print temp
-        logging.info(" ---------------------------------------------------------------------")
-        tmp=max(temp)
+  temp = []
+  # check sensors in laptop # not systen specific! great
+  st1="cat /proc/acpi/ibm/thermal | awk '{ for (i=2; i<=NF; i++) printf("
+  st1=st1+('"%s\\n" , $i)}')+("'")
+  f=os.popen(st1)
+  for i in f.readlines():
+    intw = float(i)
+    temp.append(intw)
+  maxtemp=max(temp)
+  print temp
+  print maxtemp
 
 
 
-        time.sleep(deamonTIME)
+  time.sleep(deamonTIME)
